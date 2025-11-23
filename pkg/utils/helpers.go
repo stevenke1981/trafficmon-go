c8cc7d88bf899c97154d33d58c874e310d5f0138
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2025-11-24 01:35:17 +0800

    Add utility functions for version info and interface validation

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -1,1 +1,14 @@
+package utils
 
+import "fmt"
+
+// GetVersionInfo returns formatted version information
+func GetVersionInfo(version, buildDate string) string {
+	return fmt.Sprintf("Version: %s | Build: %s", version, buildDate)
+}
+
+// ValidateInterface checks if network interface exists
+func ValidateInterface(iface string) bool {
+	// Simple validation - in real implementation, check system interfaces
+	return iface != ""
+}
