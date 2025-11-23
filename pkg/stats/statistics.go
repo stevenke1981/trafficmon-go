e73a1a67c34405ca2ab509ecb78abd968448a56c
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2025-11-24 00:52:59 +0800

    Update statistics.go

--- pkg/stats/statistics.go
+++ pkg/stats/statistics.go
@@ -1,1 +1,33 @@
+package stats
 
+import (
+    "encoding/json"
+    "fmt"
+    "time"
+)
+
+type TrafficStats struct {
+    Timestamp    time.Time         `json:"timestamp"`
+    TotalPackets int64             `json:"total_packets"`
+    TotalBytes   int64             `json:"total_bytes"`
+    Protocols    map[string]int64  `json:"protocols"`
+    Interface    string            `json:"interface"`
+}
+
+func (ts *TrafficStats) ToJSON() string {
+    data, err := json.MarshalIndent(ts, "", "  ")
+    if err != nil {
+        return fmt.Sprintf(`{"error": "%v"}`, err)
+    }
+    return string(data)
+}
+
+func (ts *TrafficStats) ToString() string {
+    return fmt.Sprintf(
+        "Time: %s | Packets: %d | Bytes: %d | Interface: %s",
+        ts.Timestamp.Format("2006-01-02 15:04:05"),
+        ts.TotalPackets,
+        ts.TotalBytes,
+        ts.Interface,
+    )
+}
